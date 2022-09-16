63af16ddd61222b1a23fb1a539b4da1de3e41890
niketic95 <niketic95@gmail.com>
2022-09-16 22:29:51 +0200

    Configurations
    - disable icons in NvimTree
    - enable mouse integration

--- init.lua
+++ init.lua
@@ -10,15 +10,43 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.guifont = { 'MonoLisa Custom', 'h12' }
 vim.opt.termguicolors = true
+vim.go.mouse = 'a'
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
 
+-- Load plugins
 require('plugins')
 
 -- Since gruvbox's an addon colorscheme add after plugins loaded
 vim.cmd([[colorscheme gruvbox]])
 
-require('nvim-tree').setup{}
-
+require('nvim-tree').setup({
+	renderer = {
+		indent_markers = {
+			enable = true
+		},
+		icons = {
+			show = {
+				file = false,
+				folder = false,
+				folder_arrow = false,
+				git = false
+			}
+		}
+	}
+})
+require('neogit').setup()
 require('lualine').setup()
 require('gitsigns').setup()
 require('hop').setup()
-require('bufferline').setup()
+require('bufferline').setup({
+	options = {
+		buffer_close_icon = 'X',
+		close_icon = 'X',
+		color_icons = false,
+		show_buffer_icons = false
+	}
+})
+
+
+
