4a8699aee4a787bc9dbab79b66781f988e41cdeb
Stefan Nicetin <stefan.nicetin@tttech-auto.com>
2022-09-20 13:58:13 +0200

    Options for vscode

--- init.lua
+++ init.lua
@@ -14,39 +14,38 @@ vim.go.mouse = 'a'
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Load plugins
-require('plugins')
+if not (vim.g.vscode) then
+	-- Load plugins
+	require('plugins')
 
--- Since gruvbox's an addon colorscheme add after plugins loaded
-vim.cmd([[colorscheme gruvbox]])
+	-- Since gruvbox's an addon colorscheme add after plugins loaded
+	vim.cmd([[colorscheme gruvbox]])
 
-require('nvim-tree').setup({
-	renderer = {
-		indent_markers = {
-			enable = true
-		},
-		icons = {
-			show = {
-				file = false,
-				folder = false,
-				folder_arrow = false,
-				git = false
+	require('nvim-tree').setup({
+		renderer = {
+			indent_markers = {
+				enable = true
+			},
+			icons = {
+				show = {
+					file = false,
+					folder = false,
+					folder_arrow = false,
+					git = false
+				}
 			}
 		}
-	}
-})
-require('neogit').setup()
-require('lualine').setup()
-require('gitsigns').setup()
-require('hop').setup()
-require('bufferline').setup({
-	options = {
-		buffer_close_icon = 'X',
-		close_icon = 'X',
-		color_icons = false,
-		show_buffer_icons = false
-	}
-})
-
-
-
+	})
+	require('neogit').setup()
+	require('lualine').setup()
+	require('gitsigns').setup()
+	require('hop').setup()
+	require('bufferline').setup({
+		options = {
+			buffer_close_icon = 'X',
+			close_icon = 'X',
+			color_icons = false,
+			show_buffer_icons = false
+		}
+	})
+end
